699898f381c71476ac07e9c5fb5b9be4044856e8
avdim <avdim@mail.ru>
2020-09-22 10:48:30 +0300

    browser solutions

---
+++ src/jsMain/kotlin/com/sample/SolutionBonusBrowserApi.kt
@@ -0,0 +1,12 @@
+package com.sample
+
+import react.RBuilder
+
+interface SolutionBonusBrowserApi {
+    fun renderBonusesAndRefillButton(react: RBuilder)
+
+    fun renderBonusCount(react: RBuilder)
+
+    fun renderBonusToggle(react: RBuilder)
+
+}
