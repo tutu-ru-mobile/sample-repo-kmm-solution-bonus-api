b8bc63dcbaade7bc19e7809352ec0d6a44da1c10
avdim <avdim@mail.ru>
2020-09-26 23:13:51 +0300

    idea solutions

---
+++ src/ideaMain/kotlin/com/sample/SolutionBonusIdeaApi.kt
@@ -0,0 +1,7 @@
+package com.sample
+
+interface SolutionBonusIdeaApi {
+    fun renderBonusesAndRefillButton(builder: IdeaPanelBuilder)
+    fun renderBonusCount(builder: IdeaPanelBuilder)
+    fun renderBonusToggle(builder: IdeaPanelBuilder)
+}
